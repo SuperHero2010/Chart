fb90dd66ee176f24b13f51e4ab1d6edf853bbea6
SuperHero2010 <156646084+SuperHero2010@users.noreply.github.com>
2025-08-03 16:01:02 +0700

    Add files via upload

---
+++ mods/Dissociation-V3/stages/Scopoliosis.lua
@@ -0,0 +1,10 @@
+function onCreate()
+
+    makeLuaSprite('ScopolioBG', 'ScopolioBG', -12000, -6500)
+    addGlitchEffect('ScopolioBG', 2, 2)
+    scaleObject('ScopolioBG', 5, 5)
+    setScrollFactor('ScopolioBG', 0.9, 0.9);
+	
+    addLuaSprite('ScopolioBG', false)
+
+end
